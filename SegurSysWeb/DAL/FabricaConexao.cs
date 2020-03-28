94a7c024c490084352d2e7bca2332a1764ef3c58
Gabriel Jesus de Sena <gabrielsena587@gmail.com>
2020-03-28 18:57:56 -0300

    Create FabricaConexao.cs

---
+++ SegurSysWeb/DAL/FabricaConexao.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class FabricaConexao
+    {
+
+        private static SqlConnection cn ;
+
+        private static string stringConexao = @"Server=DESKTOP-2ITDEA2\SQLEX;
+            Database=Segursys;User Id= sa; Password=123456";
+    
+
+        public static SqlConnection getConexao()
+        {
+            try
+            {
+                cn = new SqlConnection(stringConexao);
+                return cn;
+
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+    }
+}
