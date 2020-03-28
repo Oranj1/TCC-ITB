409a23c723c9b85d6640268ecf9f34b32fc35573
Gabriel Jesus de Sena <gabrielsena587@gmail.com>
2020-03-28 18:44:52 -0300

    Create frmQrCode.Designer.cs

---
+++ SegurSys/SegurSys/frmQrCode.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace SegurSys
+{
+    partial class frmQrCode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmQrCode));
+            this.pcQrCode = new System.Windows.Forms.PictureBox();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.lblNomeArea = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pcQrCode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcQrCode
+            // 
+            this.pcQrCode.Location = new System.Drawing.Point(12, 27);
+            this.pcQrCode.Name = "pcQrCode";
+            this.pcQrCode.Size = new System.Drawing.Size(400, 400);
+            this.pcQrCode.TabIndex = 0;
+            this.pcQrCode.TabStop = false;
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnVoltar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVoltar.ForeColor = System.Drawing.Color.Red;
+            this.btnVoltar.Location = new System.Drawing.Point(345, 459);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(77, 24);
+            this.btnVoltar.TabIndex = 8;
+            this.btnVoltar.Text = "&Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImprimir.Location = new System.Drawing.Point(12, 433);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(104, 42);
+            this.btnImprimir.TabIndex = 9;
+            this.btnImprimir.Text = "&Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            this.btnImprimir.Click += new System.EventHandler(this.btnImprimir_Click);
+            // 
+            // lblNomeArea
+            // 
+            this.lblNomeArea.AutoSize = true;
+            this.lblNomeArea.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.lblNomeArea.Location = new System.Drawing.Point(12, 4);
+            this.lblNomeArea.Name = "lblNomeArea";
+            this.lblNomeArea.Size = new System.Drawing.Size(157, 18);
+            this.lblNomeArea.TabIndex = 10;
+            this.lblNomeArea.Text = "QrCode para o ponto  ";
+            // 
+            // frmQrCode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.CancelButton = this.btnVoltar;
+            this.ClientSize = new System.Drawing.Size(426, 488);
+            this.Controls.Add(this.lblNomeArea);
+            this.Controls.Add(this.btnImprimir);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.pcQrCode);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmQrCode";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Qr Code Gerado!";
+            this.Load += new System.EventHandler(this.frmQrCode_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pcQrCode)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcQrCode;
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.Button btnImprimir;
+        public System.Windows.Forms.Label lblNomeArea;
+    }
+}
