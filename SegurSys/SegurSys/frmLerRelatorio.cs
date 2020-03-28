734e441c185c8706819156a8a1097b90f65b8ae2
Gabriel Jesus de Sena <gabrielsena587@gmail.com>
2020-03-28 18:55:18 -0300

    Create frmLerRelatorio.cs

---
+++ SegurSys/SegurSys/frmLerRelatorio.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace SegurSys
+{
+    public partial class frmLerRelatorio : Form
+    {
+        public frmLerRelatorio()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQuitar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void frmLerRelatorio_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
