21ef48dacd7a9bb2c4a78f04101a7743f06d490e
Gabriel Jesus de Sena <gabrielsena587@gmail.com>
2020-03-28 18:49:42 -0300

    Create frmMessageDialog.cs

---
+++ SegurSys/SegurSys/frmMessageDialog.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SegurSys
+{
+    public partial class frmMessageDialog : Form
+    {
+
+        public frmMessageDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNemPensar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmMessageDialog_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSimEuQuero_Click(object sender, EventArgs e)
+        {
+            if (pcbQuestion.Visible == false)
+            {
+                frmAdm.sair = true;
+                frmSeguranca.sair = true;
+            }
+            else
+            {
+                frmRotas.verificador = true;
+                frmPesquisar.verificador = true;
+            }
+            this.Close();
+        }
+
+        private void pcClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
